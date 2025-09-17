41e00bac752227a8a7b07d87d102546ed57dee9a
Coolaa <chanhoilok0604@gmail.com>
2025-09-17 22:05:09 +0800

    fix(particles): correct particle positions relative to emitter
    - Add static ParticleEmitter reference to ParticleVariable class
    - Implement logic to adjust particle positions based on emitter's last global position
    - Handle cases where particle position or rotation is relative to the emitter
    - Update particle position variables to use the adjusted previous position

--- src/client/java/mchorse/bbs_mod/particles/ParticleVariable.java
+++ src/client/java/mchorse/bbs_mod/particles/ParticleVariable.java
@@ -3,15 +3,12 @@ package mchorse.bbs_mod.particles;
 import mchorse.bbs_mod.math.Variable;
 import mchorse.bbs_mod.particles.emitter.Particle;
 import mchorse.bbs_mod.particles.emitter.ParticleEmitter;
+import org.joml.Matrix3f;
+import org.joml.Vector3d;
+import org.joml.Vector3f;
 
 public class ParticleVariable extends Variable {
-    public ParticleVariable(String name, double value) {
-        super(name, value);
-    }
-
-    public ParticleVariable(String name, String value) {
-        super(name, value);
-    }
+    public static ParticleEmitter emitter;
 
     public ParticleVariable(String name) {
         super(name, getParticleVariableValue(name, ParticleEmitter.evaluationParticle.get()));
@@ -27,6 +24,20 @@ public class ParticleVariable extends Variable {
             return 0.0;
         }
         name = name.replace("v.", "variable.");
+        Vector3d prevPosition = new Vector3d(particle.prevPosition);
+        if (emitter != null) {
+            if (particle.age != 0 && !(particle.relativePosition && particle.relativeRotation)) {
+                Vector3d lastGlobal = emitter.lastGlobal;
+                prevPosition.sub(lastGlobal);
+            }
+            if (!particle.relativePosition && particle.relativeRotation) {
+                Matrix3f inverseRotation = new Matrix3f(particle.matrix).invert();
+                Vector3f tempVec = new Vector3f();
+                tempVec.set(prevPosition);
+                inverseRotation.transform(tempVec);
+                prevPosition.set(tempVec);
+            }
+        }
         return switch (name) {
             case "variable.particle_index" -> particle.index;
             case "variable.particle_age" -> particle.getAge(0);
@@ -35,9 +46,9 @@ public class ParticleVariable extends Variable {
             case "variable.particle_random_2" -> particle.random2;
             case "variable.particle_random_3" -> particle.random3;
             case "variable.particle_random_4" -> particle.random4;
-            case "variable.particle_x" -> particle.prevPosition.x;
-            case "variable.particle_y" -> particle.prevPosition.y;
-            case "variable.particle_z" -> particle.prevPosition.z;
+            case "variable.particle_x" -> prevPosition.x;
+            case "variable.particle_y" -> prevPosition.y;
+            case "variable.particle_z" -> prevPosition.z;
             case "variable.particle_displacement" -> particle.position.length();
             case "variable.particle_vx" -> particle.speed.x;
             case "variable.particle_vy" -> particle.speed.y;
