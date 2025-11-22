8bd0a2298ca7597392cc3456aa7136b812e6ead1
Coolaa <chanhoilok0604@gmail.com>
2025-11-22 22:23:35 +0800

    Implemented quaternion rotation interpolation (Just in case, not in use)

--- src/main/java/mchorse/bbs_mod/cubic/CubicModelAnimator.java
+++ src/main/java/mchorse/bbs_mod/cubic/CubicModelAnimator.java
@@ -6,12 +6,14 @@ import mchorse.bbs_mod.cubic.data.model.Model;
 import mchorse.bbs_mod.cubic.data.model.ModelGroup;
 import mchorse.bbs_mod.math.molang.expressions.MolangExpression;
 import mchorse.bbs_mod.utils.interps.IInterp;
+import mchorse.bbs_mod.utils.interps.Interpolation;
 import mchorse.bbs_mod.utils.interps.Interpolations;
 import mchorse.bbs_mod.utils.interps.Lerps;
 import mchorse.bbs_mod.utils.keyframes.BezierUtils;
 import mchorse.bbs_mod.utils.keyframes.KeyframeChannel;
 import mchorse.bbs_mod.utils.keyframes.KeyframeSegment;
 import mchorse.bbs_mod.utils.pose.Transform;
+import org.joml.Quaterniond;
 import org.joml.Vector3d;
 
 public class CubicModelAnimator
@@ -28,6 +30,50 @@ public class CubicModelAnimator
 
         return output;
     }
+
+    public static Vector3d interpolateQuaternion(Vector3d output, KeyframeChannel<MolangExpression> x, KeyframeChannel<MolangExpression> y, KeyframeChannel<MolangExpression> z, float frame, double defaultValue) {
+        // Find segments for each axis at the given frame
+        KeyframeSegment<MolangExpression> segX = x.findSegment(frame);
+        KeyframeSegment<MolangExpression> segY = y.findSegment(frame);
+        KeyframeSegment<MolangExpression> segZ = z.findSegment(frame);
+
+        if (segX == null || segY == null || segZ == null) {
+            return new Vector3d(defaultValue);
+        }
+        // Snatch rotations
+        double startX = segX.a.getValue().get();
+        double startY = segY.a.getValue().get();
+        double startZ = segZ.a.getValue().get();
+
+        double endX = segX.b.getValue().get();
+        double endY = segY.b.getValue().get();
+        double endZ = segZ.b.getValue().get();
+
+
+        Interpolation interp = segX.b.getInterpolation();
+        // Convert to quaternions
+        Quaterniond startQuad = new Quaterniond().rotateXYZ(startX, startY, startZ);
+        Quaterniond endQuad = new Quaterniond().rotateXYZ(endX, endY, endZ);
+
+        // Interpolate rotations
+        if (interp.getInterp() == Interpolations.BEZIER || interp.getInterp() == Interpolations.HERMITE || interp.getInterp() == Interpolations.CUBIC) {
+            BezierUtils.getQuaternion(startQuad, endQuad,
+                    segX.a.getTick(), segX.b.getTick(),
+                    segX.a.rx, segX.a.ry,
+                    segX.b.lx, segX.b.ly,
+                    segX.x
+            ).getEulerAnglesXYZ(output);
+            // Return euler
+            return output;
+        }
+        double factor = interp.interpolate(IInterp.context.set(0, 0, 1, 0, segX.x));
+        startQuad.slerp(endQuad, factor);
+
+        // Convert back to euler
+        startQuad.getEulerAnglesXYZ(output);
+        return output;
+    }
+
 
     private static double interpolateSegment(KeyframeSegment<MolangExpression> segment, double defaultValue)
     {
@@ -97,9 +143,11 @@ public class CubicModelAnimator
 
     private static void applyGroupAnimation(ModelGroup group, AnimationPart animation, float frame, float blend)
     {
+        // Interpolation
         Vector3d position = interpolateList(p, animation.x, animation.y, animation.z, frame, 0D);
         Vector3d scale = interpolateList(s, animation.sx, animation.sy, animation.sz, frame, 1D);
         Vector3d rotation = interpolateList(r, animation.rx, animation.ry, animation.rz, frame, 0D);
+        // Vector3d rotation = interpolateQuaternion(r, animation.rx, animation.ry, animation.rz, frame, 0D);
 
         scale.sub(1, 1, 1);
 
@@ -109,6 +157,7 @@ public class CubicModelAnimator
         Transform initial = group.initial;
         Transform current = group.current;
 
+        // Blend/hold between multiple poses?
         current.translate.x = Lerps.lerp(current.translate.x, (float) position.x + initial.translate.x, blend);
         current.translate.y = Lerps.lerp(current.translate.y, (float) position.y + initial.translate.y, blend);
         current.translate.z = Lerps.lerp(current.translate.z, (float) position.z + initial.translate.z, blend);
