eeefe5f15d8044f06025572c077a82a31d567708
Coolaa <chanhoilok0604@gmail.com>
2025-12-07 15:15:35 +0800

    Implemented multi-light rendering

---
+++ src/client/java/mchorse/bbs_mod/forms/LightManager.java
@@ -0,0 +1,75 @@
+package mchorse.bbs_mod.forms;
+
+import coolaa.util.iris.SSBOUtils;
+import net.irisshaders.iris.gl.buffer.ShaderStorageBuffer;
+import org.lwjgl.opengl.GL45C;
+
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LightManager {
+    private static final LightManager instance = new LightManager();
+    private static final boolean enabled = isIrisInstalled();
+    public static ShaderStorageBuffer lightsSSBO = null;
+    private final List<Float> lightAttribs = new ArrayList<>();
+
+
+    public LightManager() {
+        if (!enabled) return;
+        initializeBuffer();
+    }
+
+    private static boolean isIrisInstalled() {
+        try {
+            Class.forName("net.irisshaders.iris.Iris");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    private void initializeBuffer() {
+        try {
+            lightsSSBO = SSBOUtils.setupRelativeBuffer(8, 13, 1, 1); // max index 96
+        } catch (Throwable e) {
+            // Handle case where buffer creation fails
+            lightsSSBO = null;
+        }
+    }
+
+    public static LightManager get() {
+        return instance;
+    }
+
+    public void add(List<Float> attribs) {
+        this.lightAttribs.addAll(attribs);
+    }
+
+    public void clear() {
+        this.lightAttribs.clear();
+    }
+
+    public void pushBuffer() {
+        {
+            if (!enabled) return;
+            if (lightsSSBO == null) initializeBuffer();
+            if (lightsSSBO == null) return;
+
+            // init buffer
+            lightsSSBO.bind();
+            FloatBuffer buffer = org.lwjgl.BufferUtils.createFloatBuffer(1024 + this.lightAttribs.size() + 4);
+
+            // pack data
+            buffer.put(new float[]{(float) this.lightAttribs.size() /16, 0f, 0f, 0f});
+            buffer.put(new float[1024]);
+            for (float val : this.lightAttribs) {
+                buffer.put(val);
+            }
+            buffer.flip();
+
+            // Push buffer
+            GL45C.glBufferData(GL45C.GL_SHADER_STORAGE_BUFFER, buffer, GL45C.GL_STATIC_DRAW);
+        }
+    }
+}
