c385debd0ca8829b5e55581ac757824ade92b3c7
Coolaa <chanhoilok0604@gmail.com>
2025-12-07 15:15:21 +0800

    Implemented proper multi-light rendering

--- src/client/java/mchorse/bbs_mod/forms/renderers/LightFormRenderer.java
+++ src/client/java/mchorse/bbs_mod/forms/renderers/LightFormRenderer.java
@@ -1,6 +1,7 @@
 package mchorse.bbs_mod.forms.renderers;
 
 import coolaa.util.iris.SSBOUtils;
+import mchorse.bbs_mod.forms.LightManager;
 import mchorse.bbs_mod.forms.forms.LightForm;
 import mchorse.bbs_mod.graphics.texture.Texture;
 import mchorse.bbs_mod.resources.Link;
@@ -8,38 +9,13 @@ import mchorse.bbs_mod.ui.framework.UIContext;
 import net.irisshaders.iris.gl.buffer.ShaderStorageBuffer;
 import org.joml.AxisAngle4f;
 import org.joml.Vector3f;
-import org.lwjgl.opengl.GL45C;
-
-import java.nio.FloatBuffer;
+import java.util.List;
 
 public class LightFormRenderer extends FormRenderer<LightForm> {
     public static final Link LIGHT_PREVIEW = Link.assets("textures/light.png");
 
-    private static final boolean enabled = isIrisInstalled();
-    public static ShaderStorageBuffer lightsSSBO = null;
-
-    private static boolean isIrisInstalled() {
-        try {
-            Class.forName("net.irisshaders.iris.Iris");
-            return true;
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
-
     public LightFormRenderer(LightForm form) {
         super(form);
-        if (!enabled) return;
-        initializeBuffer();
-    }
-
-    private void initializeBuffer() {
-        try {
-            lightsSSBO = SSBOUtils.setupRelativeBuffer(8, 1, 3, 2); // max index 96
-        } catch (Throwable e) {
-            // Handle case where buffer creation fails
-            lightsSSBO = null;
-        }
     }
 
     @Override
@@ -56,27 +32,20 @@ public class LightFormRenderer extends FormRenderer<LightForm> {
 
     @Override
     protected void render3D(FormRenderingContext context) {
-        if (!enabled) return;
-        if (lightsSSBO == null) initializeBuffer();
-        if (lightsSSBO == null) return;
-
         // get data
         LightForm form = this.form;
         int color = form.color.get().getRGBColor();
         Vector3f pos = context.stack.peek().getPositionMatrix().getTranslation(new Vector3f());
         Vector3f dir = context.stack.peek().getNormalMatrix().getRotation(new AxisAngle4f()).transform(new Vector3f(0,0,-1));
 
+        LightManager.get().add(List.of(
+                pos.x, pos.y, pos.z, (float) color,
+                dir.x, dir.y, dir.z, (float) form.type.get(),
+                form.intensity.get(), form.angle.get(), form.spread.get(), form.radius.get(),
+                form.attenuation.get(), 0f, 0f, 0f
+        ));
+        LightManager.get().pushBuffer();
 
-        // init buffer
-        lightsSSBO.bind();
-        FloatBuffer buffer = org.lwjgl.BufferUtils.createFloatBuffer(13);
-        // pack data
-        buffer.put(new float[]{pos.x, pos.y, pos.z, color, dir.x, dir.y, dir.z, form.type.get(),
-                               form.intensity.get(), form.angle.get(), form.spread.get(), form.radius.get(),
-                               form.attenuation.get()
-        });
-        buffer.flip();
-
-        GL45C.glBufferData(GL45C.GL_SHADER_STORAGE_BUFFER, buffer, GL45C.GL_STATIC_DRAW);
+        // TODO: render as ui element in f3
     }
 }
